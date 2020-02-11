d2a20308a45a753484a35217a22f50d02669c5c0
Complicated <cl3896@columbia.edu>
2020-02-11 13:57:54 -0500

    organize code

--- LaserBoxFile/LaserBox.py
+++ LaserBoxFile/LaserBox.py
@@ -4,20 +4,15 @@
 
 def main():
     ####################################################################################################
-    # write the svg definition
-    Boxfile.write(
-        "<svg viewBox=\"0 0 100 100\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n"
-    )
-
-    # show the boundary
-    Boxfile.write(
-        "\t<rect x=\"0\" y=\"0\" width=\"100%\" height=\"100%\" stroke=\"black\" stroke-width=\"1\" fill=\"none\"/>\n"
-    )
+    # constants for later use
+    DIM_LIMIT = 2
+    THIK_LIMIT = 0.1
 
     ####################################################################################################
     # print the user instruction
     print("Welcome to the Square generator!")
-    print("The square needs to be at least 5\" in all dimensions.")
+    print("The card borad need to be at least 0.1\" in thickness.")
+    print("The Box needs to be at least 2\" in all dimensions.")
     print(
         "Please follow the instruction to generate a Box shape. :) Have Fun!")
     print("#########################################")
@@ -30,17 +25,21 @@ def main():
             length = float(input('Enter the length of the Box: '))
             width = float(input('Enter the width of the Box: '))
             height = float(input('Enter the height of the Box: '))
+            thickness = float(input('Enter the thickness of the card borad: '))
         except ValueError:
-            print("Sorry, I didn't understand that. Please enter a number.")
+            print(
+                "Sorry, The parameters seems not right. Please enter valid numbers."
+            )
             continue
         else:
-            if width < 5 or height < 5:
+            if width < DIM_LIMIT or height < DIM_LIMIT or height < DIM_LIMIT or thickness < THIK_LIMIT:
                 print(
-                    "One or Both of the numbers was/were too small. Try again. (The box needs to be at least 5\" in all dimensions.)"
+                    "One or Both of the numbers was/were too small. Try again."
                 )
                 continue
             else:
                 print("Input Successs!")
+                print("#########################################")
                 break
 
     ####################################################################################################
@@ -49,13 +48,27 @@ def main():
         "\t<rect x=\"calc(50%% - %f/2)\" y=\"calc(50%% - %f/2)\" width=\"%f\" height=\"%f\" stroke=\"black\" stroke-width=\"0.25\" fill=\"none\"/>\n"
         % (width, height, width, height))
 
-    # close up the file
-    Boxfile.write("</svg>")
 
-
+####################################################################################################
+# set the program as main and write file
 if __name__ == "__main__":
 
-    with open("LaseBox.svg", "w") as Boxfile:
+    with open("LaseBox_0.svg", "w") as Boxfile:
+
+        # write the svg definition
+        Boxfile.write(
+            "<svg viewBox=\"0 0 1000 1000\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n"
+        )
+
+        # show the boundary
+        Boxfile.write(
+            "\t<rect x=\"0\" y=\"0\" width=\"100%\" height=\"100%\" stroke=\"black\" stroke-width=\"1\" fill=\"none\"/>\n"
+        )
+
+        # main program
         main()
+
+        # close up the file
+        Boxfile.write("</svg>")
 
     print("File generated successfully! Check your root folder :)")
